9712d66bd47c4060995e9f0bdc38af86917a2654
Caleb R <cjr4912@gmail.com>
2020-06-29 14:41:00 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,2 @@
 def valid_move
-  if position_taken? (board, index)
-  taken = nil
-  if (board[index] == " " || board[index] == "" || board[index] == nil)
-    taken = false
-  else 
-    taken = true
-    
+  if
