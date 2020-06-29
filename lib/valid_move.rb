c7f6c0b42a6ca608314c5c69183182f8c5e13712
Caleb R <cjr4912@gmail.com>
2020-06-29 14:45:55 +0000

    Automatically backed up by Learn

---
+++ lib/valid_move.rb
@@ -0,0 +1,20 @@
+def valid_move(board, index)
+  spot = index.to_i - 1
+  if position_taken?(board, index) && spot.between?(0,8)
+    true
+    
+  else
+    false
+    
+  end
+end
+
+
+def position_taken? (board, index)
+  taken = nil
+  if (board[index] == " " || board[index] == "" || board[index] == nil)
+    taken = false
+  else 
+    taken = true
+  end
+end 
